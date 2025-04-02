8ad9a898300262880c7454f184a21de907012cde
Mohammad Adnan <65945728+adnandotdev@users.noreply.github.com>
2025-04-02 05:43:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+	const age = document.getelementById("age")
+	const name = document.getelementById("name")
+	if(!name || !age){
+		alert("Please enter valid details")
+	}
+	else{
+		new Promise((resolve, reject) => {
+			setTimeOut(() => {
+				if(parseInt(age)>18){
+					resolve(`Welcome, ${name}. You can Vote`)
+				} 
+				else{
+					reject(`Oh sorry ${name}. You aren't old enough`)
+				}
+			})
+		})
+		.then((message) => alert(message))
+	}
+})
